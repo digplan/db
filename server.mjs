e86f8142b81e1689d35d02d1aedb86b05d6a75e1
digital planets <digplan@users.noreply.github.com>
2022-09-20 07:17:57 -0500

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,19 +1,18 @@
 import 'http' from 'node:http'
 import { writeFileSync } from 'node:fs'
-import schema from './schema.mjs'
+import Schema from './schema.mjs'
 import db from './db.mjs'
 const { debug } = process.env
-
-const validate = new schema()
-const { _queries as queries } = validate
+const { _queries as queries, validate } = new Schema()
 
 http.createServer(({method, headers, url}, s) => {
   try {
     console.log(method, url)
-    const [k, v] = url.slice(-2)
-    if(debug) console.log('url is ', [k, v])
+    const [null, prefix, action, param] = url.split('/')
+    if(debug) console.log('url is ', prefix, action, param)
     
     if(method == 'GET') {
+      if(debug) console.log(`GET request for key ${param}`)
       if(v && !k && db[v]) 
         return s.end(JSON.stringify(db[v]))
       if(k && v && queries[k])
@@ -25,7 +24,7 @@ http.createServer(({method, headers, url}, s) => {
       data = validate(data, data.type)
       db[k] = data
       writeFileSync('./db.mjs', 'export default ' + JSON.stringify(db, null, 2))
-      return JSON.stringify(db[k])          
+      return s.end('ok')         
     }
                    
     if(method == 'DELETE' && v && db[v])
@@ -33,12 +32,13 @@ http.createServer(({method, headers, url}, s) => {
       
     if(url === '/client.js') {
       return s.end(`
-         window.db = {}
-         window.db.get = async (k) => await (await fetch(`/${k}`)).json()
-         window.db.query = async (name, p = '') => await (await fetch(`/${name}/${p}`)).json()
-         window.db.insert = async (k, o) => await (await fetch(`/${k}`, {method: 'POST', headers: {data: o}})).json()
-         window.db.update = async (k, o) => await (await fetch(`/${k}`), {method: 'PUT', headers: {data: o}}).json()
-         window.db.delete = async (id) => await (await fetch(`/${id}`, {method: 'DELETE'})).json()
+         window.db = async (action, o = {}, param) => {
+            const f = await fetch(`/api/${param}`, {method: action, headers: {data: JSON.stringify(o)}})
+            const j = await f.json()
+            if(action.match(/POST|PUT/i) && j !== 'ok')
+              throw Error(`error: ${action} returned ${j}`)
+            return j
+         }
       `) 
     }
                    
