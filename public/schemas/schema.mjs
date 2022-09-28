0e351ade8606fb421dc9a4045f9ea8a4be5a0e80
cb <digplan@outlook.com>
2022-09-28 10:50:41 -0500

    date and color types

--- public/schemas/schema.mjs
+++ public/schemas/schema.mjs
@@ -1,15 +1,22 @@
 const schema = {
-  "_fieldtype_date": {
+
+  _fieldtype_date: {
     create: x => new Date().toISOString(),
     enforce: x => (new Date(x) !== 'invalid date')
   },
-  "Base": {
+  _fieldtype_color: {
+    enforce: s => s.match(/^#[A-F0-9]{6}/)
+  },
+
+  Base: {
     "id": "string",
     "name": "string",
     "type": "string",
     "created": "date",
-    "updated": "date"
+    "updated": "date",
+    "color": "color"
   }
+
 }
 
 export default schema
