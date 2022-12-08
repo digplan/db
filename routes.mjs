dd87742e1aad8cc1614362061052da3642027271
cb <digplan@outlook.com>
2022-12-08 15:56:16 -0600

    x

--- routes.mjs
+++ routes.mjs
@@ -1,5 +1,8 @@
 import FS from 'node:fs'
-const db = JSON.parse(FS.readFileSync('./db.json').toString('utf8'))
+
+const name = `./${process.env.TENANT || 'db'}.json`
+if(!FS.existsSync(name)) FS.writeFileSync(name, '{}')
+const db = JSON.parse(FS.readFileSync(name).toString('utf8'))
 const tokens = {'mytoken': true}
 
 export default {
@@ -11,17 +14,17 @@ export default {
 
     insert: ({ key }, s, data) => {
         db[key] = data
-        FS.writeFileSync('./db.json', JSON.stringify(db, null, 2))
+        FS.writeFileSync(name, JSON.stringify(db, null, 2))
     },
     update: ({ key }, s, data) => {
         if (!db[key]) throw Error('not found')
         db[key] = data
-        FS.writeFileSync('./db.json', JSON.stringify(db, null, 2))
+        FS.writeFileSync(name, JSON.stringify(db, null, 2))
     },
     delete: ({ tok, key }) => {
         if (!db[key]) throw Error('not found')
         delete db[key]
-        FS.writeFileSync('./db.json', JSON.stringify(db, null, 2))
+        FS.writeFileSync(name, JSON.stringify(db, null, 2))
     },
     get: ({ key }) => {
         return db[key]
